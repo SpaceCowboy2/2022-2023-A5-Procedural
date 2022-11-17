24d6012ff7021303abf6e2a0ca20c3fe09243964
unknown <julien.dor3@gmail.com>
2022-11-17 09:45:30 +0100

    [ADD] Abstract room Class

---
+++ Assets/Scripts/ProceduralRoom/Room.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Room : MonoBehaviour
+{
+    protected virtual void GenerateRoom()
+    {
+
+    }
+
+    protected virtual void Randomize()
+    {
+
+    }
+}
